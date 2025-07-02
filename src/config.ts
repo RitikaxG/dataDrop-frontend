da5ba34d52793bdf4c4cb4101cbbdaec42d3b0e2
Ritikag33 <ritikag5533@gmail.com>
2025-07-02 19:36:37 +0530

    updated backend url

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
-export const BACKEND_URL  = "http://localhost:3000";
+// export const BACKEND_URL  = "http://localhost:3000";
 // export const FRONTEND_URL = "http://localhost:5173";
 export const FRONTEND_URL = "https://datadrop.ritikaxg.co.in/"
+export const BACKEND_URL = "https://data-drop-backend.onrender.com"
